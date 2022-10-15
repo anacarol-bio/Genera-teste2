4470344a68e5e85a5ac79062d180b95ad940fcab
anacarol-bio <99566555+anacarol-bio@users.noreply.github.com>
2022-10-15 18:01:07 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-
 x = ["ACB", "ASW", "BEB", "CDX", "CEU", "CHB", "CHS", "CLM", "ESN", "FIN", "GBR", "GIH", "GWD", "IBS", "ITU", "JPT", "KHV", "LWK", "MSL", "MXL", "PEL", "PJL", "PUR", "STU", "TSI", "YRI"]
 y = [123, 112, 144, 109, 183, 108, 171, 148, 173, 105, 107, 113, 180, 162, 118, 105, 124, 116, 128, 107, 130, 158, 150, 128, 112, 186]
 
